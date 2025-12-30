2a3034eae65d7fdbd7510b48872bb9a01fb6004f
kajaldhamija1998-alt <kajaldhamija1998@gmail.com>
2025-12-30 21:47:30 +0530

    Update verdict_engine.py

--- verdict_engine.py
+++ verdict_engine.py
@@ -1,24 +1,32 @@
 def final_verdict(z_score, o_score, stress, firm_type, p_bankruptcy):
 
-    # Ohlson override – if default probability is high
+    # Bankruptcy override
     if o_score > 0.5:
         return "🚨 Very High Bankruptcy Risk (Ohlson Default Zone)"
 
+    # PUBLIC MANUFACTURING
     if firm_type == "public_manufacturing":
         if z_score > 2.99:
             return "✅ Financially Stable"
-        if z_score >= 1.8:
+        elif z_score >= 1.8:
             return "⚠️ Grey Zone – Monitor Closely"
-        return "⚠️ High Risk of Financial Distress"
+        else:
+            return "⚠️ High Risk of Financial Distress"
 
-    if firm_type == "private_manufacturing":
+    # PRIVATE MANUFACTURING
+    elif firm_type == "private_manufacturing":
         if z_score > 2.9:
             return "✅ Financially Stable"
-        return "⚠️ High Risk of Financial Distress"
+        elif z_score >= 1.23:
+            return "⚠️ Grey Zone – Monitor Closely"
+        else:
+            return "⚠️ High Risk of Financial Distress"
 
-    if firm_type == "non_manufacturing":
+    # NON MANUFACTURING
+    else:
         if z_score > 2.6:
             return "✅ Financially Stable"
-        return "⚠️ High Risk of Financial Distress"
-
-    return "Classification Error"
+        elif z_score >= 1.1:
+            return "⚠️ Grey Zone – Monitor Closely"
+        else:
+            return "⚠️ High Risk of Financial Distress"
